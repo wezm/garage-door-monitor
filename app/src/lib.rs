35659d992b2fb7d8123f65e7de52f91ccf4832d5
Wesley Moore <wes@wezm.net>
2021-09-13 15:37:21 +1000

    Move http server to its own module

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,5 +1,15 @@
 pub mod alert;
 mod door_state;
+pub mod http;
 pub mod led;
 
+use std::time::Instant;
+
 pub use door_state::{AtomicDoorState, DoorState};
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+pub struct State {
+    pub door_state: DoorState,
+    pub open_since: Option<Instant>,
+    pub notified_at: Option<Instant>,
+}
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,26 +1,16 @@
-use std::borrow::Cow;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{mpsc, Arc, RwLock};
 use std::time::{Duration, Instant};
-use std::{io, thread};
+use std::{io, process, thread};
 
-use json::object;
 use rppal::gpio::{Gpio, InputPin, OutputPin};
-use tiny_http::{Response, Server};
 
-use garage_door_monitor::{alert, led, DoorState};
+use garage_door_monitor::{alert, http, led, DoorState, State};
 
 const DOOR_PIN: u8 = 20; // header pin 38
 const LED_PIN: u8 = 21; // header pin 40
 const ONE_SECOND: Duration = Duration::from_secs(1);
-const HTML: &str = include_str!("home.html");
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-struct State {
-    door_state: DoorState,
-    open_since: Option<Instant>,
-    notified_at: Option<Instant>,
-}
+const SERVER_ADDR: (&str, u16) = ("0.0.0.0", 8888);
 
 fn main() -> Result<(), io::Error> {
     let term = Arc::new(AtomicBool::new(false));
@@ -39,25 +29,28 @@ fn main() -> Result<(), io::Error> {
     // GPIO thread
     // The GPIO thread is only spawned if we were able to acquire the pins
     // If a physical inspection of the device shows no flashing then this state should be obvious.
-    if let Ok((door, mut led)) = pins {
-        let term = Arc::clone(&term);
-        let thread = thread::spawn(move || {
-            while !term.load(Ordering::Relaxed) {
-                let door_state = door.read().into();
-                tx.send(door_state).expect("send error"); // TODO: Work out how to handle this best
+    match pins {
+        Ok((door, mut led)) => {
+            let term = Arc::clone(&term);
+            let thread = thread::spawn(move || {
+                while !term.load(Ordering::Relaxed) {
+                    let door_state = door.read().into();
+                    tx.send(door_state).expect("send error"); // TODO: Work out how to handle this best
 
-                match door_state {
-                    DoorState::Closed => led::flash(&mut led, 1),
-                    DoorState::Open => led::flash(&mut led, 2),
-                    DoorState::Unknown => led::flash(&mut led, 3),
+                    match door_state {
+                        DoorState::Closed => led::flash(&mut led, 1),
+                        DoorState::Open => led::flash(&mut led, 2),
+                        DoorState::Unknown => led::flash(&mut led, 3),
+                    }
+                    std::thread::sleep(ONE_SECOND);
                 }
-                std::thread::sleep(ONE_SECOND);
-            }
-            eprintln!("GPIO thread exiting");
-        });
-        threads.push(thread);
-    } else {
-        eprintln!("unable to set up GPIO")
+                eprintln!("GPIO thread exiting");
+            });
+            threads.push(thread);
+        }
+        Err(err) => {
+            eprintln!("Unable to set up GPIO: {}", err)
+        }
     }
 
     // State management thread
@@ -123,8 +116,20 @@ fn main() -> Result<(), io::Error> {
         threads.push(thread);
     }
 
+    // Start HTTP server
+    let server = match http::Server::new(SERVER_ADDR) {
+        Ok(server) => Arc::new(server),
+        Err(err) => {
+            eprintln!(
+                "Unable to start http server on {}:{}: {}",
+                SERVER_ADDR.0, SERVER_ADDR.1, err
+            );
+            process::exit(1);
+        }
+    };
+    eprintln!("http server running on {}:{}", SERVER_ADDR.0, SERVER_ADDR.1);
+
     // HTTP server shutdown thread
-    let server = Arc::new(Server::http("0.0.0.0:8088").unwrap()); // FIXME: unwrap
     {
         let term = Arc::clone(&term);
         let server = Arc::clone(&server);
@@ -132,59 +137,14 @@ fn main() -> Result<(), io::Error> {
             while !term.load(Ordering::Relaxed) {
                 std::thread::sleep(5 * ONE_SECOND);
             }
-            server.unblock();
+            server.shutdown();
             eprintln!("server thread exiting");
         });
         threads.push(thread);
     }
 
-    // HTTP server
-    let json = "Content-type: application/json; charset=utf-8"
-        .parse::<tiny_http::Header>()
-        .unwrap();
-    let html_content = "Content-type: text/html; charset=utf-8"
-        .parse::<tiny_http::Header>()
-        .unwrap();
-    eprintln!("server ready and waiting");
-    for request in server.incoming_requests() {
-        let response = match request.url() {
-            "/" => {
-                let current_state = { *state.read().unwrap() };
-                let status = match current_state.door_state {
-                    DoorState::Open => {
-                        let duration = current_state
-                            .open_since
-                            .map(|opened| {
-                                let now = Instant::now();
-                                let duration = now.duration_since(opened);
-                                let formatter = timeago::Formatter::new();
-                                Cow::from(formatter.convert(duration))
-                            })
-                            .unwrap_or_else(|| Cow::from("at an unknown time"));
-                        String::from(format!("🔴 Opened {}", duration))
-                    }
-                    DoorState::Closed => String::from("🟢 Closed"),
-                    DoorState::Unknown => String::from("🔵 Unknown"),
-                };
-                let html = HTML.replace("$doorstate$", &status);
-                Response::from_string(html).with_header(html_content.clone())
-            }
-            "/door.json" => {
-                let now = Instant::now();
-                let current_state = state.read().unwrap();
-                let obj = object! {
-                    state: current_state.door_state.to_string(),
-                    secs_since_notified: current_state.notified_at.map(|notified| now.duration_since(notified).as_secs()),
-                    open_for: current_state.open_since.map(|opened| now.duration_since(opened).as_secs())
-                };
-                let body = json::stringify_pretty(obj, 2);
-                Response::from_string(body).with_header(json.clone())
-            }
-            _ => Response::from_string("Not found").with_status_code(404),
-        };
-
-        request.respond(response).unwrap(); // FIXME: unwrap
-    }
+    // Handle HTTP requests
+    server.handle_requests(state);
 
     for thread in threads {
         let _ = thread.join();
